0b7ecf143bbda332c60fd2cd44789a9753fd0773
Hamil05 <hamilton2005aguirre@gmail.com>
2025-02-28 00:00:50 -0600

    Add files via upload

---
+++ Parcial_MVC/Models/Dado.cs
@@ -0,0 +1,10 @@
+﻿namespace Parcial_MVC.Models
+{
+    public class Dado
+    {
+        private static Random random = new Random();
+        public int NumeroGenerado { get; set; }
+        public double Altura { get; set; }
+        public double TiempoCaida { get; private set; }
+    }
+}
