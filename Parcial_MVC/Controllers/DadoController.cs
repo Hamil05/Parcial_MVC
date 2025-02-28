7b350a6e68d0e030e3fe7939faddea530bca4178
Hamil05 <hamilton2005aguirre@gmail.com>
2025-02-28 00:00:03 -0600

    Add files via upload

---
+++ Parcial_MVC/Controllers/DadoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Parcial_MVC.Models;
+
+namespace Parcial_MVC.Controllers
+{
+    public class DadoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calcular(Dado obdado)
+        {
+            Random rnd = new Random();
+
+            obdado.NumeroGenerado = rnd.Next(1, 10);
+
+            return View("Resultado", obdado);
+        }
+    }
+}
