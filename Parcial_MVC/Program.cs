9589b9e2d5c14a26384b86eb53c3c7a55461ab4b
Hamil05 <hamilton2005aguirre@gmail.com>
2025-02-28 00:03:40 -0600

    Update Program.cs

--- Parcial_MVC/Program.cs
+++ Parcial_MVC/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Dado}/{action=Index}/{id?}");
 
 app.Run();
